bb73921dedaf97c179276cd942792d031db974ce
maxencemottard <mottard.maxence@gmail.com>
2021-05-25 22:54:42 +0200

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
+const useSwagger = (app: INestApplication) => {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Quiches Wars Api Documentation')
+    .setDescription('The Quiches Wars offcial API documentation')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api/docs', app, document);
+};
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  useSwagger(app);
 
   await app.listen(80);
-}
+};
 bootstrap();
