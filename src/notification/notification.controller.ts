c6747e509691208f5c56620019869b84d9839ab1
Nicolas Laroche <44775996+Laroche-Nicolas@users.noreply.github.com>
2021-06-27 23:38:49 +0200

    Remove test route and autonotification when a user subscribe (#4)

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -16,24 +16,5 @@ export class NotificationController {
   @Post('save')
   async saveSubscription(@Body() body: SaveSubscriptionDto) {
     await this.applicationService.saveSubscription(body);
-    await this.applicationService.sendNotification({
-      userId: body.userId,
-      title: 'Backend notification registration successful',
-      message:
-        "Ceci est un test de notification retournée pour signaler que l'utilisateur est bien enregistré au stream des notifs",
-    });
-  }
-
-  @Post('test-notif')
-  async sendTestNotification(@Body() body) {
-    console.log('New notif request with body: ', body);
-
-    await this.applicationService.sendNotification({
-      userId: body.userId,
-      title: 'Backend notification test',
-      message:
-        "Ceci est un test de notification retournée pour signaler que l'utilisateur est bien enregistré au stream des notifs",
-    });
-    return 'notification send successfully';
   }
 }
